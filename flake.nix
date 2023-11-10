28a1fe9b2bb18c3d9a41a2c77dd648b4f3ab861d
Valentin Herrmann <me@valentin-herrmann.de>
2023-11-10 13:28:38 +0100

    Switch minlog to dev branch

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
   inputs = {
-    minlogSrc.url = "git+https://www.math.lmu.de/~minlogit/git/minlog.git";
+    minlogSrc.url =
+      "git+https://www.math.lmu.de/~minlogit/git/minlog.git?ref=dev";
     minlogSrc.flake = false;
   };
   outputs = { self, ... }@inputs: {
-
     nixosModules.container = import ./nix/container.nix { inherit inputs; };
   };
 }
